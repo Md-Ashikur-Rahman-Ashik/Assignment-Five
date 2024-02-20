3502faf73348859c95283b6ec4f74a858a8efa82
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-02-20 21:07:36 +0600

    Creating click function to seats

--- scripts/script.js
+++ scripts/script.js
@@ -14,3 +14,24 @@ function scrollDown(){
 }
 
 scrollDown();
+
+// Creating a function to change color onClick to a seat
+// This function will also change the available seats
+function changeColor(elementId){
+    const selectedButton = document.getElementById(elementId);
+    selectedButton.addEventListener("click", function(){
+        selectedButton.style.backgroundColor = "#1DD100";
+
+        const seatAvailable = document.getElementById("availableSeat");
+        const seatText = seatAvailable.innerText;
+    })
+}
+
+changeColor("A1");
+changeColor("A2");
+changeColor("A3");
+changeColor("A4");
+changeColor("B1");
+changeColor("B2");
+changeColor("B3");
+changeColor("B4");
